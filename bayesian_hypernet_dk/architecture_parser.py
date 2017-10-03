09a7e267a834f3c57bd1046cfc254668bc91c36b
David Krueger <davidscottkrueger@gmail.com>
2017-10-03 08:57:04 -0400

    first commit

---
+++ bayesian_hypernet_dk/architecture_parser.py
@@ -0,0 +1,10 @@
+"""
+WIP:
+    this would provide a generic way to specify an architecture, which can then be interpretted into a primary net, OR a hypernet...
+
+All we really need, ATM is to pass information about the layer shapes, tho, I think...
+
+"""
+
+
+
